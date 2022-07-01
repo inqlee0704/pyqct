2038f787840ba397fd95f8beb226f5a7fbb5f1cf
inqlee0704 <inqlee0704@gmail.com>
2022-06-30 21:49:20 -0500

    [Update] It works for one case by inputting dir path

--- utils/convert_dcm2img.py
+++ utils/convert_dcm2img.py
@@ -4,9 +4,6 @@
 # Use ProjSubjList to check if zunu_vida-ct.img exists
 # if not, it will generate one. 
 # ----------------------------------------------------
-# 20211220, In Kyu Lee
-# ----------------------------------------------------
-
 import os
 import pandas as pd
 from DCM2IMG import DCMtoVidaCT
@@ -14,31 +11,36 @@ import sys
 from tqdm.auto import tqdm
 
 def convert_dcm2img():
-    ProjSubjList_path = str(sys.argv[1]) # '/data4/inqlee0704/ENV18PM_ProjSubjList_cleaned.in'
-    df = pd.read_csv(ProjSubjList_path,sep='\t')
-    missing_path = []
-    missing_dcm = []
-    pbar = tqdm(range(len(df)))
-    for i in pbar:
-        case_path = df.loc[i,'ImgDir']
-        if os.path.exists(case_path):
-            if not os.path.exists(os.path.join(case_path,'zunu_vida-ct.img')):
-                try:
-                    DCMtoVidaCT(case_path)
-                except:
-                    missing_dcm.append(case_path)
-        else:
-            missing_path.append(case_path)
-    print()
-    print("***************************")
-    print('Paths not found: ')
-    print("***************************")
-    print(missing_path)
-    print()
-    print("***************************")
-    print('DCM not found: ')
-    print("***************************")
-    print(missing_dcm)
+    root_path = str(sys.argv[1]) # '/data4/inqlee0704/ENV18PM_ProjSubjList_cleaned.in'
+    if os.path.isdir(root_path):
+        case_path = root_path
+        DCMtoVidaCT(case_path)
+
+    else:
+        df = pd.read_csv(root_path,sep='\t')
+        missing_path = []
+        missing_dcm = []
+        pbar = tqdm(range(len(df)))
+        for i in pbar:
+            case_path = df.loc[i,'ImgDir']
+            if os.path.exists(case_path):
+                if not os.path.exists(os.path.join(case_path,'zunu_vida-ct.img')):
+                    try:
+                        DCMtoVidaCT(case_path)
+                    except:
+                        missing_dcm.append(case_path)
+            else:
+                missing_path.append(case_path)
+        print()
+        print("***************************")
+        print('Paths not found: ')
+        print("***************************")
+        print(missing_path)
+        print()
+        print("***************************")
+        print('DCM not found: ')
+        print("***************************")
+        print(missing_dcm)
 
 
 if __name__ == "__main__":
