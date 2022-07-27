e5191405ba131f2f3194875f9ca98f6d69e852d3
In Kyu <inqlee0704@gmail.com>
2022-07-27 10:00:10 -0500

    [Update] Check file paths & argument is used to check data.

--- utils/check_data.py
+++ utils/check_data.py
@@ -1,13 +1,8 @@
 import os
-from dotenv import load_dotenv
-import time
-import random
-import wandb
+import sys
 import pandas as pd
 from medpy.io import load
-import numpy as np
 from tqdm.auto import tqdm
-
 
 import SimpleITK as sitk
 
@@ -17,8 +12,8 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def check_files(subjlist):
-    subj_paths = subjlist.loc[:, "ImgDir"].values
+def check_files(df):
+    subj_paths = df.loc[:, "ImgDir"].values
     img_path_ = [
         os.path.join(subj_path, "zunu_vida-ct.img") for subj_path in subj_paths
     ]
@@ -32,30 +27,24 @@ def check_files(subjlist):
             print(mask_path_[i], "Not exists")
 
 
-def get_config():
 
-    config = wandb.config
-    config.data_path = os.getenv("VIDA_PATH")
-    config.in_file = "ENV18PM_ProjSubjList_IN0_train_20211129.in"
-    config.in_file_valid = "ENV18PM_ProjSubjList_IN0_valid_20211129.in"
+def main():
 
-    return config
+    data_path = str(sys.argv[1]) # /data
 
+    df = pd.read_csv(data_path, sep="\t")
+    # Check if exists
+    check_files(df)
 
-if __name__ == "__main__":
-    load_dotenv()
-    c = get_config()
-
-    df_train = pd.read_csv(os.path.join(c.data_path, c.in_file), sep="\t")
-    df_valid = pd.read_csv(os.path.join(c.data_path, c.in_file_valid), sep="\t")
-
-    # Train #
-    pbar = tqdm(enumerate(df_train.ImgDir), total=len(df_train))
-    # pbar = tqdm(enumerate(df_valid.ImgDir), total=len(df_valid))
-    for i, path in pbar:
-        img, _ = load(os.path.join(path, "zunu_vida-ct.img"))
-        lung, _ = load(os.path.join(path, "ZUNU_vida-lung.img.gz"))
+    # Check dimensions of image and mask
+    pbar = tqdm(enumerate(df.ImgDir), total=len(df))
+    for i, subj_path in pbar:
+        img, _ = load(os.path.join(subj_path, "zunu_vida-ct.img"))
+        lung, _ = load(os.path.join(subj_path, "ZUNU_vida-lung.img.gz"))
         if img.shape != lung.shape:
             print(f"Dimension not match")
-            print(path)
+            print(subj_path)
             print(f"img: {img.shape}  | lung: {lung.shape}")
+
+if __name__ == "__main__":
+    main()
